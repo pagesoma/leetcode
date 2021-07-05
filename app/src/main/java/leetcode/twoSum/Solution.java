201e258470d4f7f005cfb6a53201be660b505ce4
pagesoma <pagesoma@gmail.com>
2021-07-06 01:44:10 +0900

    solve twoSum

---
+++ app/src/main/java/leetcode/twoSum/Solution.java
@@ -0,0 +1,25 @@
+package leetcode.twoSum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+
+  public int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> map = new HashMap<>();
+
+    int num = 0;
+    int loopCount = nums.length;
+
+    for (int i = 0; i < loopCount; i++) {
+      num = target - nums[i];
+      if (map.get(num) != null) {
+        return new int[] {map.get(num), i};
+      }
+
+      map.put(nums[i], i);
+    }
+
+    return null;
+  }
+}
